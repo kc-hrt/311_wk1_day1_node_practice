c3f76a00357e584307487ae62af00c91eee6ab5b
kc-hrt <kaseyh@pm.me>
2021-06-29 20:39:24 -0700

    all tests pass

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,17 +1,18 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  return (num % 2 === 0) ? true : false;
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  return arr.reduce((total, amount) => total + amount);
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  return (arr.reduce((total, amount) => { return total + amount }) == sum) ? true : false;
 }
+
 
 module.exports = {
   isEven,
